4d1ed8b82ba894e3761aea4f2155e9167d0abdee
brkylmz7 <mburakylmz@outlook.com>
2025-12-09 21:24:51 +0300

    Fixed

--- src/navigation/BottomTabs.tsx
+++ src/navigation/BottomTabs.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { View, Image } from 'react-native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { useAppSelector } from '../hooks';
 
@@ -19,19 +20,229 @@ export default function BottomTabs() {
   const role = useAppSelector(state => state.auth.role);
 
   return (
-    <Tab.Navigator screenOptions={{ headerShown: false }}>
+    <Tab.Navigator
+      screenOptions={{
+        headerShown: false,
+        tabBarShowLabel: true,
+        tabBarActiveTintColor: '#000',
+        tabBarInactiveTintColor: '#777',
+        tabBarStyle: {
+          backgroundColor: '#FFD500',
+          height: 80,
+          paddingBottom: 8,
+          paddingTop: 8,
+        },
+        tabBarLabelStyle: {
+          fontSize: 12,
+          fontWeight: '600',
+        },
+      }}>
       {role === 'driver' ? (
         <>
-          <Tab.Screen name="DriverHome" component={DriverHome} />
-          <Tab.Screen name="DriverJobs" component={DriverJobs} />
-          <Tab.Screen name="DriverAllJobs" component={DriverAllJobs} />
+          {/* DRIVER HOME */}
+          <Tab.Screen
+            name="DriverHome"
+            component={DriverHome}
+            options={{
+              title: 'Anasayfa',
+              tabBarIcon: ({ focused }) => (
+                <View
+                  style={{
+                    paddingHorizontal: focused ? 20 : 0,
+                    paddingVertical: focused ? 15 : 0,
+                    backgroundColor: focused ? '#F7B500' : 'transparent',
+                    borderRadius: 99,
+                    justifyContent: 'center',
+                    alignItems: 'center',
+                  }}>
+                  <Image
+                    source={require('../../assets/icons/home.png')}
+                    style={{
+                      width: 35,
+                      height: 35,
+                      tintColor: focused ? 'black' : '#444',
+                    }}
+                  />
+                </View>
+              ),
+            }}
+          />
+
+          {/* DRIVER ALL JOBS */}
+          <Tab.Screen
+            name="DriverAllJobs"
+            component={DriverAllJobs}
+            options={{
+              title: 'Piyasa İşleri',
+              tabBarIcon: ({ focused }) => (
+                <View
+                  style={{
+                    paddingHorizontal: focused ? 20 : 0,
+                    paddingVertical: focused ? 10 : 0,
+                    backgroundColor: focused ? '#F7B500' : 'transparent',
+                    borderRadius: 99,
+                    justifyContent: 'center',
+                    alignItems: 'center',
+                  }}>
+                  <Image
+                    source={require('../../assets/icons/excavator.png')}
+                    style={{
+                      width: 35,
+                      height: 35,
+                      tintColor: focused ? 'black' : '#444',
+                    }}
+                  />
+                </View>
+              ),
+            }}
+          />
+
+          {/* DRIVER JOBS */}
+          <Tab.Screen
+            name="DriverJobs"
+            component={DriverJobs}
+            options={{
+              title: 'Seferlerim',
+              tabBarIcon: ({ focused }) => (
+                <View
+                  style={{
+                    paddingHorizontal: focused ? 20 : 0,
+                    paddingVertical: focused ? 15 : 0,
+                    backgroundColor: focused ? '#F7B500' : 'transparent',
+                    borderRadius: 99,
+                    justifyContent: 'center',
+                    alignItems: 'center',
+                  }}>
+                  <Image
+                    source={require('../../assets/icons/drive.png')}
+                    style={{
+                      width: 35,
+                      height: 35,
+                      tintColor: focused ? 'black' : '#444',
+                    }}
+                  />
+                </View>
+              ),
+            }}
+          />
         </>
       ) : (
         <>
-          <Tab.Screen name="SupplierHome" component={SupplierHome} />
-          <Tab.Screen name="SupplierAllJobs" component={SupplierAllJobs} />
-          <Tab.Screen name="MyJobs" component={MyJobs} />
-          <Tab.Screen name="SupplierVehicles" component={SupplierVehicles} />
+          {/* SUPPLIER HOME */}
+          <Tab.Screen
+            name="SupplierHome"
+            component={SupplierHome}
+            options={{
+              title: 'Anasayfa',
+              tabBarIcon: ({ focused }) => (
+                <View
+                  style={{
+                    paddingHorizontal: focused ? 20 : 0,
+                    paddingVertical: focused ? 15 : 0,
+                    backgroundColor: focused ? '#F7B500' : 'transparent',
+                    borderRadius: 99,
+                    justifyContent: 'center',
+                    alignItems: 'center',
+                  }}>
+                  <Image
+                    source={require('../../assets/icons/home.png')}
+                    style={{
+                      width: 35,
+                      height: 35,
+                      tintColor: focused ? '#FFF' : '#444',
+                    }}
+                  />
+                </View>
+              ),
+            }}
+          />
+
+          {/* SUPPLIER ALL JOBS */}
+          <Tab.Screen
+            name="SupplierAllJobs"
+            component={SupplierAllJobs}
+            options={{
+              title: 'Tüm İşler',
+              tabBarIcon: ({ focused }) => (
+                <View
+                  style={{
+                    paddingHorizontal: focused ? 20 : 0,
+                    paddingVertical: focused ? 15 : 0,
+                    backgroundColor: focused ? '#F7B500' : 'transparent',
+                    borderRadius: 99,
+                    justifyContent: 'center',
+                    alignItems: 'center',
+                  }}>
+                  <Image
+                    source={require('../../assets/icons/home.png')}
+                    style={{
+                      width: 35,
+                      height: 35,
+                      tintColor: focused ? '#FFF' : '#444',
+                    }}
+                  />
+                </View>
+              ),
+            }}
+          />
+
+          {/* SUPPLIER MY JOBS */}
+          <Tab.Screen
+            name="MyJobs"
+            component={MyJobs}
+            options={{
+              title: 'İşlerim',
+              tabBarIcon: ({ focused }) => (
+                <View
+                  style={{
+                    paddingHorizontal: focused ? 20 : 0,
+                    paddingVertical: focused ? 15 : 0,
+                    backgroundColor: focused ? '#F7B500' : 'transparent',
+                    borderRadius: 99,
+                    justifyContent: 'center',
+                    alignItems: 'center',
+                  }}>
+                  <Image
+                    source={require('../../assets/icons/home.png')}
+                    style={{
+                      width: 35,
+                      height: 35,
+                      tintColor: focused ? '#FFF' : '#444',
+                    }}
+                  />
+                </View>
+              ),
+            }}
+          />
+
+          {/* SUPPLIER VEHICLES */}
+          <Tab.Screen
+            name="SupplierVehicles"
+            component={SupplierVehicles}
+            options={{
+              title: 'Araçlarım',
+              tabBarIcon: ({ focused }) => (
+                <View
+                  style={{
+                    paddingHorizontal: focused ? 20 : 0,
+                    paddingVertical: focused ? 15 : 0,
+                    backgroundColor: focused ? '#F7B500' : 'transparent',
+                    borderRadius: 99,
+                    justifyContent: 'center',
+                    alignItems: 'center',
+                  }}>
+                  <Image
+                    source={require('../../assets/icons/home.png')}
+                    style={{
+                      width: 35,
+                      height: 35,
+                      tintColor: focused ? '#FFF' : '#444',
+                    }}
+                  />
+                </View>
+              ),
+            }}
+          />
         </>
       )}
     </Tab.Navigator>
